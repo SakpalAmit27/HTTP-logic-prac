3428d2dbea8f3a7458fc1e70869242c819058241
Sakpal Amit <amitsakpal744@gmail.com>
2024-10-14 14:23:06 +0530

    first commit , along with basic boilerplate

---
+++ index.js
@@ -0,0 +1,16 @@
+// creatting http / 
+
+const express = require('express');
+
+const app = express(); 
+
+// request methods are : GET,POST,PUT,DELETE // 
+
+
+
+
+app.get("/",(req,res) =>{
+   
+})
+
+app.listen(3000);
