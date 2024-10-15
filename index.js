2a104231ee4df5a60edf4af406a5ee41b3b0c81f
Sakpal Amit <amitsakpal744@gmail.com>
2024-10-15 08:14:08 +0530

    counting the healthy ones by iterating

--- index.js
+++ index.js
@@ -14,7 +14,20 @@ app.get("/",(req,res) => {
 
     const user_kidneys = users[0].kidneys;
 
-    res.send(user_kidneys)
+    // counting no of kidneys // 
+
+    const number_of_kidneys = kidneys.length;
+
+    let number_of_healthy_kidneys = 0; 
+
+    for(let i = 0; i<user_kidneys.length;i++){
+        if(user_kidneys[i].healthy){
+            number_of_healthy_kidneys = number_of_healthy_kidneys + 1;
+        }
+    }
+
+    
+
 
 })
 
